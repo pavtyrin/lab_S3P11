f309e47ccfece9a287c12c84bb4c957431e6f2e4
pavtyrin (director) <pavtyrin705@gmail.com>
2024-10-22 21:54:07 +0300

    Создание структуры

---
+++ lab_S3P11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+struct Geography
+{
+    int id;
+    string name;
+    string country;
+    string type;
+    int height;
+    double area;
+    int population;
+};
